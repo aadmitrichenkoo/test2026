3319f18c1797b14f209fe5424083f942c31c0a29
aadmitrichenkoo <aadmitrichenkoo@gmail.com>
2026-01-22 21:11:32 +0400

    test3

--- cypress/e2e/01.login/login.cy.js
+++ cypress/e2e/01.login/login.cy.js
@@ -1,29 +1,30 @@
-const { beforeEach } = require("node:test");
-
-// Тест на вход в систему
-
-describe('Вход в систему', () => {
+// Тест на вход в систем
+describe ('Вход в систему', () => {
     
 let testData;
 
-    before(() => {
-    cy.fixtures('test_users').then((data) => {
-    testData = data;
+before(() => {
+        cy.fixture('test_users').then((data) => {
+        testData = data;
     });
   });
 
 beforeEach(() => {
-    cy.visit(testData.baseURL)/web/index.php/auth/login
-    cy.wait(5000);
-
-})
+    cy.visit(`${testData.baseURL}/web/index.php/auth/login`);
+    cy.get('input[name="username"]').should('be.visible');
+    cy.wait(10000);
+    });
 
 // Тест успешного входа
 
-it('Успешная авторизация', () => {
-    cy.get('[class="oxd-input oxd-input--active"]').type(login);
-    cy.get('[class="oxd-input oxd-input--active"]').type(password);
+it ('Успешная авторизация', () => {
+    cy.get('input[name="username"]')
+        .type(testData.validUser.login);
+
+    cy.get('input[name="password"]')
+        .type(testData.validUser.password);
+
+    cy.get('button[type="submit"]').click();
+});
 
 })
-
-});
