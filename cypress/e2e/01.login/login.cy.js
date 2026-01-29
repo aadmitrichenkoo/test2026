eaf36ce0aede423f522dcd8c010359bf57de1969
Nastya Dm <aadmitrichenkoo@gmail.com>
2026-01-29 21:07:11 +0400

    JRT

--- cypress/e2e/01.login/login.cy.js
+++ cypress/e2e/01.login/login.cy.js
@@ -11,17 +11,32 @@ before(() => {
 
 beforeEach(() => {
     cy.visit(`${testData.baseURL}/web/index.php/auth/login`);
-    cy.get('input[name="username"]').should('be.visible');
+    cy.get('input[placeholder="Username"]').should('be.visible');
     });
 
 // Тест успешного входа
 it ('Успешная авторизация', () => {
-    cy.get('input[name="username"]')
+    cy.get('input[="Username"]')
         .type(testData.validUser.login);
 
     cy.get('input[name="password"]')
         .type(testData.validUser.password);
 
     cy.get('button[type="submit"]').click();
+    });
+
+
+// Негативный тест
+it ('Негативная авторизация', () => {
+    cy.get('input[name="username"]')
+        .type(testData.invalidUser.login);
+
+    cy.get('input[name="password"]')
+        .type(testData.invalidUser.password);
+
+    cy.get('button[type="submit"]').click();
+    cy.get('.oxd-alert-content-text')
+        .should('be.visible')
+        .and('contain', 'Invalid credentials');
+    });
 });
-})
